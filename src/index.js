a4d69936dd4a67e506c7bf765ac6dcbffdeeeba5
Robert Luo <robertxluo@gmail.com>
2022-06-23 16:13:07 -0700

    Fix eslintrc config file for project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
