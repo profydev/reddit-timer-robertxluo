1a97332f3a0e997d776a03b790b29e35676a2dbf
Robert Luo <robertxluo@gmail.com>
2022-06-23 16:36:19 -0700

    Normalize css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'normalize.css';
+import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
