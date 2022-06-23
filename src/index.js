225c2cbcb75844faaa00c9591eefe77ee03ef79c
Robert Luo <robertxluo@gmail.com>
2022-06-23 16:50:53 -0700

    Install fonts, router, set up folder structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './containers/App';
 import 'normalize.css';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/search" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
