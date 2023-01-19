5cbc87573737e168a3666f960674cc077a478f99
Robert Luo <robertxluo@gmail.com>
2023-01-19 15:02:49 -0800

    Change app title and meta description

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<>Home</>} />
+        <Route path="/search" element={<>Search</>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './containers/App';
 import 'normalize.css';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/search" element={<App />} />
-    </Routes>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
